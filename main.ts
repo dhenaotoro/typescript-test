64d662b45464fefd09343f49c759b00cd83f4144
= <=>
2022-10-25 14:01:12 +0000

    Primer commit

--- main.ts
+++ main.ts
@@ -2,4 +2,17 @@ import {Aprendiz, NivelEducativo} from './aprendiz.js';
 
 export const ap = new Aprendiz("Juan Pablo", "Reyes Gómez", "avatar.png", 30, NivelEducativo.POSTGRADO);
 
-console.log(ap);
+let apredizTable: HTMLElement = document.getElementById("aprendiz")!; //El elemento no es nulo no hay riesgo de que sea nulo porque ya existe
+
+mostrarDatosAprendiz(ap);
+
+function mostrarDatosAprendiz(aprendiz: Aprendiz): void{
+    let tbodyAprendiz = document.createElement("tbody");
+    tbodyAprendiz.innerHTML = `<tr><td colspan=2><img src="./${aprendiz.avatar}"></td></tr>
+    <tr><td>Nombres: </td><td>${aprendiz.nombres}</td></tr>  
+    <tr><td>Apellidos: </td><td>${aprendiz.apellidos}</td></tr>
+    <tr><td>Nivel educativo: </td><td>${aprendiz.nivelEducativo}</td></tr>
+    <tr><td>Edad: </td><td>${aprendiz.edad}</td></tr>
+    `
+    apredizTable.appendChild(tbodyAprendiz);
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
 import { Aprendiz, NivelEducativo } from './aprendiz.js';
 export var ap = new Aprendiz("Juan Pablo", "Reyes Gómez", "avatar.png", 30, NivelEducativo.POSTGRADO);
-console.log(ap);
+var apredizTable = document.getElementById("aprendiz"); //El elemento no es nulo no hay riesgo de que sea nulo porque ya existe
+mostrarDatosAprendiz(ap);
+function mostrarDatosAprendiz(aprendiz) {
+    var tbodyAprendiz = document.createElement("tbody");
+    tbodyAprendiz.innerHTML = "<tr><td colspan=2><img src=\"./" + aprendiz.avatar + "\"></td></tr>\n    <tr><td>Nombres: </td><td>" + aprendiz.nombres + "</td></tr>  \n    <tr><td>Apellidos: </td><td>" + aprendiz.apellidos + "</td></tr>\n    <tr><td>Nivel educativo: </td><td>" + aprendiz.nivelEducativo + "</td></tr>\n    <tr><td>Edad: </td><td>" + aprendiz.edad + "</td></tr>\n    ";
+    apredizTable.appendChild(tbodyAprendiz);
+}
